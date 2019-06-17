36b31404eb00a1c994b650ab5097429827f7667a
Eric Evans <eevans@gogoair.com>
2019-06-17 05:37:26 -0600

    Updated to take request variable from browser

--- src/hello.py
+++ src/hello.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    data = request.args.to_dict()
+    if 'name' in data.keys():
+        return f"<h1>Hello {data['name']}!</h1>"
     return '<h1>Hello World!</h1>'
